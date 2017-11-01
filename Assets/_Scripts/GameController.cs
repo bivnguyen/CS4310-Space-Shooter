3df91d44aac2d233a41258a81db861210db286b8
bivnguyen <bnguyen42@horizon.csueastbay.edu>
2017-11-01 16:23:31 -0700

    Added Score Tracker

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour 
 {
@@ -11,8 +12,13 @@ public class GameController : MonoBehaviour
 	public float startWait;
 	public float waveWait;
 
+	public Text scoreText;
+	private int score;
+
 	void Start()
 	{
+		score = 0;
+		UpdateScore ();
 		StartCoroutine (SpawnWaves());
 	}
 
@@ -29,6 +35,16 @@ public class GameController : MonoBehaviour
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
 	}
 	
 }
