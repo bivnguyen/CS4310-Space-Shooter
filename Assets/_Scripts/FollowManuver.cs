c334e75f95fc6d585d36ac1f778f9bdaa27060e9
James <2889180+jtlopez2@users.noreply.github.com>
2017-11-03 11:51:58 -0700

    Added chaser enemy
    Created a new prefab of ChaserShip, added FollowManuver script to attatch to ChaserShip prefab.

---
+++ Assets/_Scripts/FollowManuver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowManuver : MonoBehaviour {
+
+	private Rigidbody rb;
+	public Transform target;
+	public float chaseSpeed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		if (target) {
+			rb.transform.LookAt (target.position);
+			transform.Rotate (new Vector3 (0, 180, 0), Space.Self);
+			transform.position = Vector3.MoveTowards(transform.position, target.position, chaseSpeed);
+		}
+	}
+}
