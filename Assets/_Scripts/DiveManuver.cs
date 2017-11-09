36114527aff200718957375d6f5d16e53143a06e
James Lopez <robotface@gmail.com>
2017-11-09 11:41:27 -0800

    Created new dive manuver
    new dive manuver teleports back to top of screen, created new dive script, saved previous as OLD, and created a new boundary with the tag DiveBoundary for calculating when to teleport dive ship back to top

--- Assets/_Scripts/DiveManuver.cs
+++ Assets/_Scripts/DiveManuver.cs
@@ -6,43 +6,35 @@ public class DiveManuver : MonoBehaviour {
 
 	private Rigidbody rb;
 	public Transform target;			//Player ship
-	public float diveSpeed;				//Movement speed
 	private Vector3	diveTarget;			//Static player location stored
-	private Vector3 diveDestination;	//Dive return location opposite spawn location
-	private bool isDiving;				//Check for when to move to dive return location
+	private bool rotated;
 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		//Enemy ship will dive to players coordinates when enemy is spawned
 		target = GameObject.FindWithTag("Player").transform;
 		diveTarget = target.position;
+		rotated = false;
 		//Enemy ship will return to the opposite side of the game board it spawned on after diving
-		diveDestination = new Vector3 (-rb.transform.position.x, rb.transform.position.y, rb.transform.position.z);
-		isDiving = true;
 
 	}
 
 	void FixedUpdate () {
-		//Check to sdee if dive has reached the diving target and flag to return to top of game board
-		if (rb.transform.position == diveTarget) {
-			isDiving = false;
-		}
-		//Once enemy has finished its dive and return animation, begin the dive and return animation back to original spawn
-		if (rb.transform.position == diveDestination) {
+		if (target && !rotated) {
+			target = GameObject.FindWithTag("Player").transform;
 			diveTarget = target.position;
-			diveDestination = new Vector3 (-rb.transform.position.x, rb.transform.position.y, rb.transform.position.z);
-			isDiving = true;
-		}
-		//Aim towards the dive target and move while diving
-		if (target && isDiving) {
 			rb.transform.LookAt (diveTarget);
 			transform.Rotate (new Vector3 (0, 180, 0), Space.Self);
-			transform.position = Vector3.MoveTowards (rb.transform.position, diveTarget, diveSpeed);
-		} else {  //Aim towards the dive target and move while returning
-			rb.transform.LookAt (diveTarget);
-			transform.Rotate (new Vector3 (0, 180, 0), Space.Self);
-			transform.position = Vector3.MoveTowards (rb.transform.position, diveDestination, diveSpeed);
+			rotated = true;
+		} else {
+			rb.velocity = transform.forward * -5;
 		}
+	}
 
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "DiveBoundary") {
+			rotated = false;
+			rb.transform.position = new Vector3 (Random.Range (-5.9f, 5.9f), 0, 17.4f);
+		}
 	}
 }
---
+++ Assets/_Scripts/DiveManuverOLD.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiveManuverOLD : MonoBehaviour {
+
+	private Rigidbody rb;
+	public Transform target;			//Player ship
+	public float diveSpeed;				//Movement speed
+	private Vector3	diveTarget;			//Static player location stored
+	private Vector3 diveDestination;	//Dive return location opposite spawn location
+	private bool isDiving;				//Check for when to move to dive return location
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		//Enemy ship will dive to players coordinates when enemy is spawned
+		target = GameObject.FindWithTag("Player").transform;
+		diveTarget = target.position;
+		//Enemy ship will return to the opposite side of the game board it spawned on after diving
+		diveDestination = new Vector3 (-rb.transform.position.x, rb.transform.position.y, rb.transform.position.z);
+		isDiving = true;
+
+	}
+
+	void FixedUpdate () {
+		//Check to sdee if dive has reached the diving target and flag to return to top of game board
+		if (rb.transform.position == diveTarget) {
+			isDiving = false;
+		}
+		//Once enemy has finished its dive and return animation, begin the dive and return animation back to original spawn
+		if (rb.transform.position == diveDestination) {
+			diveTarget = target.position;
+			diveDestination = new Vector3 (-rb.transform.position.x, rb.transform.position.y, rb.transform.position.z);
+			isDiving = true;
+		}
+		//Aim towards the dive target and move while diving
+		if (target && isDiving) {
+			rb.transform.LookAt (diveTarget);
+			transform.Rotate (new Vector3 (0, 180, 0), Space.Self);
+			transform.position = Vector3.MoveTowards (rb.transform.position, diveTarget, diveSpeed);
+		} else {  //Aim towards the dive target and move while returning
+			rb.transform.LookAt (diveTarget);
+			transform.Rotate (new Vector3 (0, 180, 0), Space.Self);
+			transform.position = Vector3.MoveTowards (rb.transform.position, diveDestination, diveSpeed);
+		}
+
+	}
+}
