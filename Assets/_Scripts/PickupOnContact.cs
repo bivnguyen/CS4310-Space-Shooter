003fe8707037e1db053b765bf2a2f9d8a4e190ba
Lawrence <lawrence2055@gmail.com>
2017-11-21 18:49:01 -0800

    pickup
    added PickupOnContact but is not tested and i don't know if it works properly

---
+++ Assets/_Scripts/PickupOnContact.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupOnContact : MonoBehaviour {
+
+	private GameController gameController;
+
+	void Start()
+	{
+		
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent<GameController> ();
+		}
+
+		if (gameController == null) 
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+
+	}
+		
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+		{
+			return;
+		}
+
+		// some code here to makes it so that you have the power up
+		// i have no idea if this code even works though
+
+		Destroy (gameObject);
+
+	}
+
+}
