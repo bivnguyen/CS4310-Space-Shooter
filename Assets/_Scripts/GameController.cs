c12978e9c6f4ef5cf82e4c2235724d570b6ea434
James Lopez <>
2017-11-28 19:31:45 -0800

    Added button controller
    Added the button controller to name submit button, added logic to make submit button and text box invisible on game start

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -52,6 +52,8 @@ public class GameController : MonoBehaviour
 		pause = false;
         god = false;
 		UpdateScore ();
+		nameInput.gameObject.SetActive (false);
+		submitButton.gameObject.SetActive (false);
 
 	}
 
